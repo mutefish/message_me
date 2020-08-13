bfc0a8d39b1434d03a3b81347ab76c825f464869
Karen Ho <karenho008@gmail.com>
2020-08-13 15:18:33 -0400

    More styling changings, limiting messages to display

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,6 +22,15 @@ scroll_bottom = function() {
   }
 }
 
+submit_message = function() {
+  $('#message_body').on('keydown', function(e) {
+    if(e.keyCode === 13) {
+      $('button').click();
+      e.target.value = ""; 
+    };
+  });
+};
+
 $(document).on('turbolinks:load', function() {
     $('.ui.dropdown').dropdown();
     $('.message .close')
@@ -31,5 +40,6 @@ $(document).on('turbolinks:load', function() {
         .transition('fade')
       ;
     });
+    submit_message();
     scroll_bottom();
 })
